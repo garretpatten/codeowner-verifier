27c2b98ee8b0a9148c10ed11de1340f67cbaf59c
Garret Patten <garret.patten@proton.me>
2022-07-27 13:53:19 -0400

    Lay foundation for codeowners-validator action

---
+++ src/action.js
@@ -0,0 +1,16 @@
+/* GitHub Dependencies */
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+/* Node Dependencies */
+const fs = require('fs');
+
+/* I/O */
+const INPUT_README_PATH = 'codeownersPath';
+const OUTPUT_TIMESTAMP = 'time';
+
+function run() {
+	console.log('this is a test');
+}
+
+run();
