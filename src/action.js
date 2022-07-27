ad6034ad906e4d4cf8c71803b1c1b028d51e20dd
Garret Patten <garret.patten@proton.me>
2022-07-27 16:13:10 -0400

    Try incorporting action to generate diff

--- src/action.js
+++ src/action.js
@@ -7,32 +7,69 @@ const fs = require('fs');
 
 /* I/O */
 const INPUT_CODEOWNERS_PATH = 'codeownersPath';
+const INPUT_CHANGED_FILES = 'changedFiles';
 const OUTPUT_TIMESTAMP = 'timestamp';
 
 const repoName = github.context.payload.repository.full_name.split('/')[1];
+
+// TODO: Determine if given filepath has
+// a corresponding codeowners entry
+function isFilepathInCodeowners(filepath, codeownersFilepaths) {
+	console.log('isFilepathInCodeowners has been called with:');
+	console.log(filepath);
+	return true;
+}
 
 function run() {
 	console.log('Running codeowners-validator action for the ' + repoName + ' repository...');
 
 	const codeownersPath = core.getInput(INPUT_CODEOWNERS_PATH);
+	const changedFiles = core.getInput(INPUT_CHANGED_FILES);
 
 	const codeownersMetadata = fs.readFileSync(codeownersPath, 'utf8');
 	const codeownersLines = codeownersMetadata.split('\n');
 
-	const codeownersEntries = [];
+	let codeownerEntry;
+	const codeownersMap = new Map();
 	for (let codeownerLine of codeownersLines) {
 		// Filter out comments and blank lines
 		if (codeownerLine.substring(0,1) !== '#'
-			|| codeownerLine.length > 1) {
-			codeownersEntries.push(codeownerLine);
+			&& codeownerLine.length > 1) {
+				codeownerEntry = codeownerLine.split(' ');
+				codeownersMap.set(
+					codeownerEntry[0],
+					codeownerEntry[1]
+				);
 		}
 	}
 
-	console.log('These are the codeownersLines');
-	console.log(codeownersLines);
-	console.log('');
-	console.log('These are the codeownersEntries');
-	console.log(codeownersEntries);
+	/* TODO:
+		- Iterate through changed files
+		- If a changed file is identified that is not
+		  in the codeownersFilepaths, then the
+		  check should fail
+	*/
+	const codeownersFilepaths = codeownersMap.keys();
+	for (let filepath of changedFiles) {
+		if (!isFilepathInCodeowners(filepath, codeownersFilepaths)) {
+			// core.setFailed('Updates have been made that do not have a corresponding codeowners entry');
+			console.log('An updated filepath has been found that is not in the CODEOWNERS file');
+			console.log(filepath);
+		}
+	}
+
+	/* TODO:
+		- Iterate through the codeownersMap
+		- Ensure that every filepath is
+			pointing to a valid GitHub
+			Team or individual
+		- If an invalid GitHub Team or
+			individual is found, then
+			the check should fail
+	*/
+	for (let key of codeownersMap) {
+
+	}
 
 	core.setOutput(
 		OUTPUT_TIMESTAMP,
